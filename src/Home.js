80e6856b1bea072efa023a0cf1768f393c1a28f4
jungsunbeen <tjqls041103@gmail.com>
2024-07-19 23:59:48 +0900

    route 라이브러리 설치

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <Header>마이페이지</Header>
+    <body>
+        <Bingos>
+            <Bingo></Bingo>
+        </Bingos>
+        <Infos></Infos>
+    </body>
+    </>
+  )
+}
+
+export default Home
+
